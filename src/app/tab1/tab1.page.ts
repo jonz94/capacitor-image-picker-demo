65d78096d7af5582b48f0d2ded1eb7fb73aff759
jonz94 <jody16888@gmail.com>
2022-10-14 14:50:39 +0800

    feat: compare with other capacitor plugins

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Camera } from '@capacitor/camera';
+import { Filesystem } from '@capacitor/filesystem';
+import { FilePicker } from '@capawesome/capacitor-file-picker';
 import { ImagePicker } from '@jonz94/capacitor-image-picker';
 
 @Component({
@@ -7,14 +10,54 @@ import { ImagePicker } from '@jonz94/capacitor-image-picker';
   styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  webPaths: any[] = [];
+
   constructor() {}
 
-  async present() {
+  async present(limit = 1) {
     try {
-      const result = await ImagePicker.present();
+      const { images } = await ImagePicker.present({ limit });
+      console.log(images);
+      this.webPaths = images.map((image) => image.webPath);
+
+      for (const image of images) {
+        const contents = await Filesystem.readFile({ path: image.path });
+        console.log(contents);
+
+        const file = await this.convertBase64DataToFile(
+          contents.data,
+          image.mimeType,
+          image.path.split('/')[image.path.split('/').length - 1],
+        );
+        console.log(file);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async pickImages() {
+    try {
+      const { photos } = await Camera.pickImages({ limit: 1 });
+      console.log(photos);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async pickFiles() {
+    try {
+      const result = await FilePicker.pickFiles();
       console.log(result);
     } catch (error) {
       console.log(error);
     }
+  }
+
+  async convertBase64DataToFile(base64Data: string, mimeType: string, filename = '') {
+    const url = `data:${mimeType};base64,${base64Data}`;
+    return fetch(url)
+      .then((res) => res.arrayBuffer())
+      .then((buf) => new File([buf], filename));
   }
 }
