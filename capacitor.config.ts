40364e4eec08aef6cdbe087103ce9b30e0c8e157
jonz94 <jody16888@gmail.com>
2025-05-06 09:59:46 +0800

    feat: `npx cap init`

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'dev.jonz94.capacitorjs.demo.imagepicker',
+  appName: 'Image Picker Demo',
+  webDir: 'www'
+};
+
+export default config;
